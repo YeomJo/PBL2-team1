f10c57abbcf50becbeaa99baa46214f8ac12dbe8
Leejooyeol <gametyr@naver.com>
2020-09-16 15:05:24 +0900

    main change

--- main/MyApp.java
+++ main/MyApp.java
@@ -1,5 +1,6 @@
 package main;
 import base.*;
+import java.util.StringTokenizer;
 
 /**
  * Write a description of class MyApp here.
@@ -10,14 +11,17 @@ import base.*;
 public class MyApp
 {
     public static int[] makeToken(String str){
-        int[] result_array = {};
+        int[] result_array = new int[6];
+        int changed_value = 0;
         StringTokenizer st = new StringTokenizer(str, ",");
         while(st.hasMoreTokens())
-            result_array.append(Integer.parseInt(st.nextToken()));
+            changed_value = Integer.parseInt(st.nextToken());
+            result_array.append(changed_value);
     }
     
-    public static Circle makeCircle(){
-        
+    public static Circle makeCircle(int x, int y, int radius){
+        Circle circle = new Circle(x, y, radius);
+        return circle;
     }
     
     public static void main(String[] args){
@@ -25,11 +29,11 @@ public class MyApp
         String data = "2,3,5,2,3,10";
         array = makeToken(data);
         Circle c1 = makeCircle(array[0], array[1], array[2]);
-        Circle c2 = makeCircla(array[3], array[4], array[5]);
+        Circle c2 = makeCircle(array[3], array[4], array[5]);
         
         System.out.println("원1 :" + c1 + "==> 면적:" + c1.getArea());
-        System.out.priltln("원2 :" + c2 + "==> 면적:" + c2.getArea());
-        if()
+        System.out.println("원2 :" + c2 + "==> 면적:" + c2.getArea());
+        if(c1.equals(c2))
             System.out.println("같은 원");
         else
             System.out.println("서로 다른 원");
