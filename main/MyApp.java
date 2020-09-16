af50f22d2022fb40acccc501bdb0fda17a55b4f9
Kim MinSoo <k21332133@naver.com>
2020-09-16 15:31:22 +0900

    update all

--- main/MyApp.java
+++ main/MyApp.java
@@ -1,23 +1,32 @@
 package main;
-import base.Circle;
+
+import base.*;
+import java.util.StringTokenizer;
+
+
 
 /**
  * Write a description of class MyApp here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2019315007 이주열)
+ * @version (2020.09.16)
  */
 public class MyApp
 {
-    public static int[] makeToken(String str){
-        int[] result_array = {};
+    public static Integer[] makeToken(String str){
+        Integer[] result_array = new Integer[6];
+        int changed_value = 0;
         StringTokenizer st = new StringTokenizer(str, ",");
-        while(st.hasMoreTokens())
-            result_array.append(Integer.parseInt(st.nextToken()));
+        for(int i = 0; i < st.countTokens(); i ++){
+            changed_value = Integer.parseInt(st.nextToken());
+            result_array[i] = changed_value;
+        }
+        return result_array;
     }
     
-    public static Circle makeCircle(){
-        
+    public static Circle makeCircle(int x, int y, int radius){
+        Circle circle = new Circle(x, y, radius);
+        return circle;
     }
     
     public static void main(String[] args){
@@ -25,10 +34,11 @@ public class MyApp
         String data = "2,3,5,2,3,10";
         array = makeToken(data);
         Circle c1 = makeCircle(array[0], array[1], array[2]);
-        Circle c2 = makeCircla(array[3], array[4], array[5]);
+        Circle c2 = makeCircle(array[3], array[4], array[5]);
         
         System.out.println("원1 :" + c1 + "==> 면적:" + c1.getArea());
-        System.out.priltln("원2 :" + c2 + "==> 면적:" + c2.getArea());
+        System.out.println("원2 :" + c2 + "==> 면적:" + c2.getArea());
+
         if(c1.equals(c2))
             System.out.println("같은 원");
         else
--- myInterface/Shape.java
+++ myInterface/Shape.java
@@ -4,8 +4,8 @@ package myInterface;
 /**
  * 여기에 Shape 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2019315015 김민수) 
+ * @version (2020.09.16)
  */
 public interface Shape
 {
