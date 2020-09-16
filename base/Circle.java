e71278f67c9da1749757ad10871cc9a92b8ae6d0
Leejooyeol <gametyr@naver.com>
2020-09-16 15:24:27 +0900

    change main

--- base/Circle.java
+++ base/Circle.java
@@ -4,8 +4,8 @@ import myInterface.Shape;
 /**
  * 여기에 Circle 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
+ * @author (2019315015 김민수) 
+ * @version (2020.09.16)
  */
 public class Circle implements Shape
 {
@@ -13,7 +13,7 @@ public class Circle implements Shape
     public int y;
     public int radius;
     
-    public void Circle(int x, int y, int radius){
+    public Circle(int x, int y, int radius){
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -21,5 +21,14 @@ public class Circle implements Shape
     
     public double getArea(){
         return radius * radius * PI;
+    }
+    
+    @Override
+    public boolean equals(Object obj){
+        Circle c = (Circle)obj;
+        if(this.x == c.x && this.y == c.y)
+            return true;
+        else
+            return false;
     }
 }
--- main/MyApp.java
+++ main/MyApp.java
@@ -1,22 +1,27 @@
 package main;
+
 import base.*;
 import java.util.StringTokenizer;
+
+
 
 /**
  * Write a description of class MyApp here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (2019315007 이주열)
+ * @version (2020.09.16)
  */
 public class MyApp
 {
-    public static int[] makeToken(String str){
-        int[] result_array = new int[6];
+    public static Integer[] makeToken(String str){
+        Integer[] result_array = new Integer[6];
         int changed_value = 0;
         StringTokenizer st = new StringTokenizer(str, ",");
-        while(st.hasMoreTokens())
+        for(int i = 0; i < st.countTokens(); i ++){
             changed_value = Integer.parseInt(st.nextToken());
-            result_array.append(changed_value);
+            result_array[i] = changed_value;
+        }
+        return result_array;
     }
     
     public static Circle makeCircle(int x, int y, int radius){
@@ -33,6 +38,7 @@ public class MyApp
         
         System.out.println("원1 :" + c1 + "==> 면적:" + c1.getArea());
         System.out.println("원2 :" + c2 + "==> 면적:" + c2.getArea());
+
         if(c1.equals(c2))
             System.out.println("같은 원");
         else
