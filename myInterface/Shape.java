e2606bae2b47cbd9c0ed7145fa7db08aa28376f9
Kim MinSoo <k21332133@naver.com>
2020-09-16 14:53:34 +0900

    make Circle, Shape

---
+++ myInterface/Shape.java
@@ -0,0 +1,14 @@
+package myInterface;
+
+
+/**
+ * 여기에 Shape 클래스 설명을 작성하십시오.
+ * 
+ * @author (작성자 이름) 
+ * @version (버전번호나 날짜)
+ */
+public interface Shape
+{
+    public final double PI = 3.14;
+    public abstract double getArea();
+}
